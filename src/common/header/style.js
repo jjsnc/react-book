d093f3651ad3627150f7c7ef5ff3d48bffd2bcbc
jjsnc <zhuowangyi@163.com>
2019-11-13 20:16:49 +0800

    styled-commont

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -2,7 +2,7 @@
 * @Author: jjsnc
 * @Date:   2019-11-13 17:24:43
 * @Last Modified by:   jjsnc
-* @Last Modified time: 2019-11-13 17:57:33
+* @Last Modified time: 2019-11-13 20:13:12
 */
 
 import styled  from 'styled-components'
@@ -31,7 +31,74 @@ export const HeaderWrapper = styled.div`
     width:960px;
     height:100%;
     margin:0 auto;
+    padding-right: 70px;
+    box-sizing:border-box;
   `
+
+  export const NavItem = styled.div`
+  height:56px;
+  line-height: 56px;
+  color:#333;
+   &.left{
+   	float:left;
+   }
+   &.right{
+   	float: right;
+   	color:#969696
+   }
+   &.active {
+   	 color: #ea6f5a
+   }
+  `
+
+
+export const NavSearch = styled.input.attrs({
+	placeholder:'搜索'
+})`
+  height:38px;
+  width:106px;
+  border:none;
+  outline: none;
+  line-height: 56px;
+  color:#333;
+  display:inline-block;
+  border-radius :19px;
+  background: #eee;
+  font-size:14px;
+  margin-left: 20px
+  margin-top:9px;
+  padding:0 20px;
+  box-sizing: border-box;
+  &.::placeholder:{
+  	color: #999;
+  }
+  `
+
+  export const Addition = styled.div`
+   position: absolute;
+   top:0;
+   right:0;
+  `
+
+  export const Button = styled.div`
+   float:right;
+   line-height:38px;
+   border-radius:19px;
+   margin-top:9px;
+   border:1px solid #ec6149;
+   margin-right: 20px;
+   padding:  0 20px;
+   font-size:14px;
+   &.reg{
+       color: #ec6149;
+   }
+   &.writting{
+   	color:#fff;
+   	background: #ec6149;
+
+   }
+  `
+
 
 
  
