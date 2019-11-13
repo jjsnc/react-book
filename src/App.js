3f4e43c0789c879651c70ee3288c9bf238dfe659
jjsnc <zhuowangyi@163.com>
2019-11-13 17:03:06 +0800

    styled-components 使用

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React ,{Component}from 'react';
 
-
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+import {Wrapper, Title } from './style'
+import {GlobalStyle} from './styleCommon.js'
+class App extends  Component{
+  render(){
+   return (
+     <div>
+       <GlobalStyle  whiteColor>  </GlobalStyle>
+       <Wrapper>
+         <Title>
+          Hello World!
+          <ul>
+            <li>001</li>
+            <li>002</li>
+            <li>003</li>
+          </ul>
+         </Title>
+       </Wrapper>
+       </div>
+     );
+  }
 }
-
 export default App;
