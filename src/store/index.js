79c3fc3d633e0703e45d30f692fd673c71dc1387
jjsnc <zhuowangyi@163.com>
2019-11-15 09:33:44 +0800

    redux-immutable

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,15 @@
 * @Author: jjsnc
 * @Date:   2019-11-14 22:34:09
 * @Last Modified by:   jjsnc
-* @Last Modified time: 2019-11-14 22:38:11
+* @Last Modified time: 2019-11-15 09:25:10
 */
 
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
+
+// redux-immutable
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,10 +2,10 @@
 * @Author: jjsnc
 * @Date:   2019-11-14 22:34:21
 * @Last Modified by:   jjsnc
-* @Last Modified time: 2019-11-15 00:28:07
+* @Last Modified time: 2019-11-15 09:26:50
 */
 
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux-immutable'
 
 import {reducer as headerReducer } from '../common/header/store/'
 
