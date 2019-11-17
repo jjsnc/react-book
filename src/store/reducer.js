5a6067823f51d885690e7d9d83829cb7f3947989
jjsnc <zhuowangyi@163.com>
2019-11-17 10:47:28 +0800

    detail

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,16 +2,17 @@
 * @Author: jjsnc
 * @Date:   2019-11-14 22:34:21
 * @Last Modified by:   jjsnc
-* @Last Modified time: 2019-11-16 17:28:41
+* @Last Modified time: 2019-11-17 10:35:05
 */
 
 import { combineReducers } from 'redux-immutable'
 
 import {reducer as headerReducer } from '../common/header/store/'
 import {reducer as homeReducer } from '../pages/home/store/'
-
+import {reducer as detailReducer} from '../pages/detail/store'
 
 export  default combineReducers({
 	header:headerReducer,
-	home:homeReducer
+	home:homeReducer,
+	detail: detailReducer
 })
