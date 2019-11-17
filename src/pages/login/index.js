9b25045b624555649309fd3b30e49ee5c7be91ac
jjsnc <zhuowangyi@163.com>
2019-11-17 11:43:03 +0800

    login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,24 +2,42 @@
 * @Author: jjsnc
 * @Date:   2019-11-16 15:29:23
 * @Last Modified by:   jjsnc
-* @Last Modified time: 2019-11-16 15:45:54
+* @Last Modified time: 2019-11-17 11:42:17
 */
 
 import React, { PureComponent } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { LoginWrapper, LoginBox, Input, Button } from './style';
+import { actionCreators } from './store';
 
 class Login extends PureComponent {
-
-
 	render() {
-		return (
-		  <div>Login</div>
-		)
+		const { loginStatus } = this.props;
+		if (!loginStatus) {
+			return (
+				<LoginWrapper>
+					<LoginBox>
+						<Input placeholder='账号' res={(input) => {this.account = input}}/>
+						<Input placeholder='密码' type='password' res={(input) => {this.password = input}}/>
+						<Button onClick={() => this.props.login(this.account, this.password)}>登陆</Button>
+					</LoginBox>
+				</LoginWrapper>
+			)
+		}else {
+			return <Redirect to='/'/>
+		}
 	}
-
 }
 
+const mapState = (state) => ({
+	loginStatus: state.getIn(['login', 'login'])
+})
 
+const mapDispatch = (dispatch) => ({
+	login(accountElem, passwordElem){
+		dispatch(actionCreators.login(accountElem.value, passwordElem.value))
+	}
+})
 
-export default connect(null, null)(Login);
+export default connect(mapState, mapDispatch)(Login);
